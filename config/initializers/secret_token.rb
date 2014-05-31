f88e6b6bf62d334684327a6965c48a1b6b8b5134
Kora Saccharin <kora.saccharin@mailoo.org>
2014-05-31 13:17:03 +0200

    Once upon a time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zbis::Application.config.secret_key_base = 'caaad0d6cd28ec2df6729f939ae6804705b1d1057c1c2ec30d069b4b3118c36017dc15e43a1ee3bf8c7ead2b31f03cb1bf1dd71f5f56ca32c4673bcbeb970c30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zbis::Application.config.session_store :cookie_store, key: '_zbis_session'
